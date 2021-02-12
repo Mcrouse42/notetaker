9334f7b633b75d712e1c980c0754404385271767
Mcrouse <matthew.crouse@temple.edu>
2021-02-12 12:41:00 -0500

    added app.use for express to access public

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require("express");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 
-
-
-
-
-
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//allows server to access files in public folder
+app.use(express.static("public"));
 
 
 app.use("/api", apiRoutes);
