5a9c8a510277a91e4beeafc1704cbc3c97480111
Mcrouse <matthew.crouse@temple.edu>
2021-02-16 15:00:20 -0500

    updated html routes

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,44 +1,18 @@
-// Routes to json data
+
+// Routes to HTML pages
 
 // Requirements
-const router = require("express").Router();
-const { findById, createNewNote, validateNote } = require("../../lib/notes");
-const { notes } = require("../../db/db.json");
+const path = require('path');
+const router = require('express').Router();
 
-// GET route for showing the notes on lefthand side
-router.get("/notes", (req, res) => {
-  let results = notes;
-  res.json(results);
+// Route for notes.html
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
-// GET route for showing note based on id
-router.get("/notes/:id", (req, res) => {
-  const result = findById(req.params.id, notes);
-  if (result) {
-    res.json(result);
-  } else {
-    res.send(404);
-  }
-});
-
-// POST route for posting a new note to the server
-router.post("/notes", (req, res) => {
-  // req.body is wher our incoming content will be
-  // Set id based on what the next index of the array will be
-  req.body.id = notes.length.toString();
-
-  // Check validation, if valid add note, if not alert user
-  if (!validateNote(req.body)) {
-    res.status(400).send("The note is not properly formatted!");
-  } else {
-    // Add note to json file and notes array in this function
-    const note = createNewNote(req.body, notes);
-    res.json(note);
-  }
-});
-
-router.delete("/notes/:id", (req, res) => {
-  console.log("you are trying to delete a note");
+// GET * should return index.html (catch all for entering the wrong/non-existent route)
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 module.exports = router;
